44b3731b366b1f9a806ebc312bc1a0d37699ab18
lindaludovisi <64600991+lindaludovisi@users.noreply.github.com>
2020-05-02 16:13:22 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -50,12 +50,12 @@ class Caltech(VisionDataset):
         '''
         #define self.db : a list containing the paths of the files of our split
         if self.split == 'train':
-            with open('train.txt') as file:
+            with open('train.txt', 'r') as file:
                 for line in file:
                     line = line.strip() #preprocess line
                     self.db.append(line) #a list of strings: every string is <category>/image_<number>
         elif self.split == 'test':
-            with open('test.txt') as file:
+            with open('test.txt', 'r') as file:
                 for line in file:
                     line = line.strip() #preprocess line
                     self.db.append(line) #a list of strings: every string is <category>/image_<number>
